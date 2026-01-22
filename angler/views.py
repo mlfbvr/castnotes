b99ea2b2acaca1e79fd9f20e4907b5b1871dceeb
Martin Lefebvre <martin@bearded-coder.com>
2026-01-22 12:52:15 -0500

    Modify profile view to return last 5 catches Display the catches returned in teh profile page

--- angler/views.py
+++ angler/views.py
@@ -1,32 +1,41 @@
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+
 def home(request):
-	"""Default view for angler app. Prompts login or registration if not authenticated."""
-	if request.user.is_authenticated:
-		return render(request, "angler/home.html")
-	return render(request, "angler/welcome.html")
+    """Default view for angler app. Prompts login or registration if not authenticated."""
+    if request.user.is_authenticated:
+        return render(request, "angler/home.html")
+    return render(request, "angler/welcome.html")
 
 
 @login_required
 def profile(request):
-	"""Display the authenticated user's profile page."""
-	return render(request, "angler/profile.html", {"user": request.user})
+    """Display the authenticated user's profile page."""
+    catches = request.user.catches.all().order_by("-catch_datetime")[:5]
+    return render(
+        request,
+        "angler/profile.html",
+        {
+            "user": request.user,
+            "total_catches_count": request.user.catches.all().count(),
+            "catches": catches,
+        },
+    )
 
 
 @login_required
 def log_catch(request):
-	"""Display the form to log a new catch."""
-	from .forms import CatchForm
+    """Display the form to log a new catch."""
+    from .forms import CatchForm
 
-	if request.method == "POST":
-		form = CatchForm(request.POST, request.FILES)
-		if form.is_valid():
-			catch = form.save(commit=False)
-			catch.user = request.user
-			catch.save()
-			return redirect("profile")
-	else:
-		form = CatchForm()
-	return render(request, "angler/log_catch.html", {"form": form})
+    if request.method == "POST":
+        form = CatchForm(request.POST, request.FILES)
+        if form.is_valid():
+            catch = form.save(commit=False)
+            catch.user = request.user
+            catch.save()
+            return redirect("profile")
+    else:
+        form = CatchForm()
+    return render(request, "angler/log_catch.html", {"form": form})
