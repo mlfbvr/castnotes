8f4f316f09ce23a78d2f859b8d0b0b1b38ddb7cb
Martin Lefebvre <martin@bearded-coder.com>
2026-01-22 13:55:51 -0500

    Clean up welcome page and remove debugging code

--- angler/admin.py
+++ angler/admin.py
@@ -5,7 +5,6 @@ from django.contrib.admin.sites import AlreadyRegistered
 # Automatically register all models in this app
 app = apps.get_app_config("angler")
 for model in app.get_models():
-    print(model)
     try:
         admin.site.register(model)
     except AlreadyRegistered:
